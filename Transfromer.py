9248ea6a24541138e5b4551f4f7ae369dc0bb67c
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-07-21 14:26:46 +0530

    Transformer

--- Transfromer.py
+++ Transfromer.py
@@ -7,3 +7,18 @@ import copy
 
 
 class MultiHeadAttention(nn.Module):
+    def __init__(self,d_model,num_heads):
+        super(MultiHeadAttention,self).__init__()
+        assert d_model%num_heads==0 ,"d-model must be divisible by num_threads"
+        self.d_model=d_model
+        self.num_heads=num_heads
+
+        self.d_k=d_model//num_heads
+
+        self.W_q=nn.Linear(d_model,d_model)
+        self.W_k=nn.Linear(d_model,d_model)
+        self.W_v=nn.linear(d_model,d_model)
+        self.W_o=nn.linear(d_model,d_model)
+
+        def Scaled_dot_product_attention(self,Q,K,V,mask=None):
+            attention_scores=torch.matmul(Q,K.transpose(-2,-1))/math.sqrt(self.d_k)
